27401ab7ad46c9c5388aef93fab01bc79989c748
Daria <tkachenko.darya95@gmail.com>
2026-03-09 17:57:10 +0200

    02-mongodb completed

---
+++ src/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import notesRoutes from './routes/notesRoutes.js';
+import { connectMongoDB } from './db/connectMongoDB.js';
+
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+await connectMongoDB();
+
+app.use(logger);
+app.use(cors());
+app.use(express.json());
+
+app.use(notesRoutes);
+
+app.use(notFoundHandler);
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
