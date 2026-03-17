bd834f01fee77841826e14d1e7f94a70437c3591
Daria <tkachenko.darya95@gmail.com>
2026-03-17 13:40:19 +0200

    fix authenticate import

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { upload } from '../middleware/multer.js';
-import authenticate from '../middleware/authenticate.js';
+import { authenticate } from '../middleware/authenticate.js';
 import { updateUserAvatar } from '../controllers/userController.js';
 
 const router = express.Router();
